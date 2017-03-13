2f82faa17e29d80e155b068607ca0d30653cc3aa
noelg <goldenchild_gt@yahoo.com>
2017-03-13 04:35:28 -0400

    video

---
+++ Multimedia/Scripts/default.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+
+$(document).ready(function () {
+    $('#play').on('click', playStop);
+    //$('#media').on('play', function () { $('#play').html('Pause'); });
+    $('#media').on('pause', function () { $('#play').html('Play'); });
+    $('#media').on('play', $('#play').html('Play'));
+});
+
+function playStop()
+{
+    var media = $('#media')[0];
+
+    if (media.paused)
+    {
+        media.play();
+    }
+    else
+    {
+        media.pause();
+    }
+}
